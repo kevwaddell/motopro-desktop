a031f8f993ed7eba5d2eb4662148b6ab860843b1
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-22 10:07:34 +0000

    Start home page styles

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<!-- TOP INTRO AND VIDEO SECTION -->
+<?php inc('hp-top-section', 'front_page'); ?>
+
+<!-- ABOUT COMPANY SECTION -->
+<?php inc('hp-about-section', 'front_page'); ?>
+
+<?php get_footer(); ?>
