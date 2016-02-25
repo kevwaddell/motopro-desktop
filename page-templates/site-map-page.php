56fe388540b63c875ae98a95b32733f37b1e774a
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-25 10:27:38 +0000

    Add page templates.

---
+++ page-templates/site-map-page.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Template Name: Sitemap page
+*/
+?>
+<?php get_header(); ?>
+<!-- Container  -->
+<div class="container">
+	<h1 class="font-cond caps"><?php the_title(); ?></h1>
+	<?php the_content(); ?>
+</div>
+<!-- Container end  -->
+<?php get_footer(); ?>
