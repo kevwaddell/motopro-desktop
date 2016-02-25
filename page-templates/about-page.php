161a1ca8e30c0ca488d81c5664a7ba2a55a4f428
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-25 15:01:52 +0000

    Add About page wide featured image styles.

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -4,10 +4,21 @@ Template Name: About page
 */
 ?>
 <?php get_header(); ?>
+
+<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
+
+<?php if ( has_post_thumbnail() ) { ?>
+	<?php inc('featured-img', 'pages'); ?>
+<?php } ?>
+
 <!-- Container  -->
 <div class="container">
-	<h1 class="font-cond caps"><?php the_title(); ?></h1>
+	<h2 class="font-cond caps pg-tag"><?php the_title(); ?></h2>
 	<?php the_content(); ?>
 </div>
 <!-- Container end  -->
+
+<?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
