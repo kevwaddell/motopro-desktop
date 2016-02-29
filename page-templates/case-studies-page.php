63c668882dfbf0d0a8e30588b0a5458ca4b20018
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-29 14:05:54 +0000

    Start case studies page.

--- page-templates/case-studies-page.php
+++ page-templates/case-studies-page.php
@@ -4,10 +4,79 @@ Template Name: Case Studies page
 */
 ?>
 <?php get_header(); ?>
+
+<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
+
+<?php 
+$pg_sections = get_field('pg_sections');
+$banner_title_active =	false;
+$our_team_active = false;
+$form_active = false;
+$quote_active = false;
+$jump2menu_items = array();
+
+if (!empty($pg_sections) ) {
+	//echo '<pre class="debug">';print_r($pg_sections);echo '</pre>';
+	
+	foreach ($pg_sections as $section) {
+		if ($section == "Banner title") {
+		$banner_title_active = true;	
+		}
+		
+		if ($section == "Our Team") {
+		$our_team_active = true;	
+		array_push($jump2menu_items, array('title' => $section, 'id' => sanitize_title($section) ) );
+		}
+		
+		if ($section == "Request a Callback") {
+		$form_active = true;	
+		array_push($jump2menu_items, array('title' => $section, 'id' => sanitize_title($section) ) );
+		}
+		
+		if ($section == "Client quote") {
+		$quote_active = true;	
+		array_push($jump2menu_items, array('title' => $section, 'id' => sanitize_title($section) ) );
+		}	
+		
+	}
+}
+?>
+
+<?php if ( has_post_thumbnail() ) { ?>
+	<?php inc('featured-img-intro', 'pages'); ?>
+<?php } ?>
+
 <!-- Container  -->
 <div class="container">
-	<h1 class="font-cond caps"><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+	<article <?php post_class(); ?>>
+		
+		<div class="row">
+			<div class="col-xs-6 col-xs-offset-3">
+				<h2 class="font-cond caps text-center pg-tag"><?php the_title(); ?></h2>
+			</div>
+		</div>
+		
+		<div class="case-studies-list">
+			
+		</div>
+		
+	</article>
 </div>
 <!-- Container end  -->
+
+<?php if ($our_team_active) { ?>
+<?php inc('our-team', 'sections'); ?>		
+<?php } ?>
+
+<?php if ($form_active) { ?>
+<?php inc('request-a-callback', 'sections'); ?>		
+<?php } ?>
+
+<?php if ($quote_active) { ?>
+<?php inc('client-quote', 'sections'); ?>		
+<?php } ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
