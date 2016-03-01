a9547ef9ef0badadc56ed36c251f1495753ce847
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-01 09:35:54 +0000

    Add client feedback section and styles.

--- page-templates/feedback-page.php
+++ page-templates/feedback-page.php
@@ -4,10 +4,67 @@ Template Name: Feedback page
 */
 ?>
 <?php get_header(); ?>
+
+<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
+
+<?php 
+$pg_sections = get_field('pg_sections');
+$banner_title_active =	false;
+$our_team_active = false;
+$form_active = false;
+$quote_active = false;
+$legal_services_active = false;
+
+if (!empty($pg_sections) ) {
+	//echo '<pre class="debug">';print_r($pg_sections);echo '</pre>';
+	
+	foreach ($pg_sections as $section) {
+		if ($section == "Banner title") {
+		$banner_title_active = true;	
+		}
+		
+		if ($section == "Request a Callback") {
+		$form_active = true;	
+		}
+		
+		if ($section == "Legal Services") {
+		$legal_services_active = true;	
+		}
+		
+	}
+}
+?>
+
+<?php if ( has_post_thumbnail() ) { ?>
+	<?php inc('featured-img-intro', 'pages'); ?>
+<?php } ?>
+
 <!-- Container  -->
 <div class="container">
-	<h1 class="font-cond caps"><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+	<article <?php post_class(); ?>>
+		
+		<div class="row">
+			<div class="col-xs-6 col-xs-offset-3">
+				<h2 class="font-cond caps text-center pg-tag"><?php the_title(); ?></h2>
+			</div>
+		</div>
+		
+		<?php inc('client-feedback', 'sections'); ?>		
+		
+	</article>
 </div>
 <!-- Container end  -->
+
+
+<?php if ($form_active) { ?>
+<?php inc('client-feedback-form', 'sections'); ?>		
+<?php } ?>
+
+<?php if ($legal_services_active) { ?>
+<?php inc('legal-services', 'sections'); ?>
+<?php } ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
