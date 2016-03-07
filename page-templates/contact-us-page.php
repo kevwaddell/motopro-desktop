8eb97bbefe3e2de0f4696e5273c03882ac792358
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-03-07 14:21:49 +0000

    Contact page sections map and styles added.

--- page-templates/contact-us-page.php
+++ page-templates/contact-us-page.php
@@ -4,10 +4,46 @@ Template Name: Contact us page
 */
 ?>
 <?php get_header(); ?>
-<!-- Container  -->
-<div class="container">
-	<h1 class="font-cond caps"><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-</div>
-<!-- Container end  -->
+
+<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
+
+<?php 
+$pg_sections = get_field('pg_sections');
+$banner_title_active =	false;
+$form_active = false;
+$map_active = false;
+
+if (!empty($pg_sections) ) {
+	//echo '<pre class="debug">';print_r($pg_sections);echo '</pre>';
+	
+	foreach ($pg_sections as $section) {
+		
+		switch ($section) {
+			case "Banner title": $banner_title_active = true;	
+			break;
+			case "Contact Form": $form_active = true;
+			break;
+			case "Location Map": $map_active = true;
+			break;
+		}
+		
+	}
+}
+?>
+
+<?php if ( has_post_thumbnail() && $banner_title_active ) { ?>
+	<?php inc('contact-us-intro', 'pages'); ?>
+<?php } ?>
+
+<?php if ($form_active) { ?>
+<?php inc('contact-us-form', 'sections'); ?>		
+<?php } ?>
+
+<?php if ($map_active) { ?>
+<?php inc('location-map', 'sections'); ?>
+<?php } ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
